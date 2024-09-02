ef60ccba5b50ef6b3ef63bd72f7e3999b5dbba87
F. AyuIqraami <161196084+AyuIqraami@users.noreply.github.com>
2024-09-02 20:13:21 +0800

    Create Day_1.java
    First Commit

---
+++ Day_1.java
@@ -0,0 +1,22 @@
+package Day1;
+
+public class BiodataDiri {
+
+    public static void main(String[] args) {
+        String nama = "F. Ayu Iqraami";
+        String nim = "D0223329";
+        String Prodi = "Informatika";
+        char kelas = 'A';
+        int angkatan = 2023;
+        String Alamat = "Malunda";
+
+        System.out.println("Nama : "+nama);
+        System.out.println("NIM : "+nim);
+        System.out.println("Prodi : "+Prodi);
+        System.out.println("Kelas : " +kelas);
+        System.out.println("Angkatan : "+angkatan);
+        System.out.println("Alamat : "+Alamat);
+
+    }
+
+}
