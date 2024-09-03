385c7de4993d522f028fca04c8b91620aaf9ec44
F. AyuIqraami <161196084+AyuIqraami@users.noreply.github.com>
2024-09-03 20:13:55 +0800

    Create Day2.java
    Printf

---
+++ Day2.java
@@ -0,0 +1,16 @@
+package Day2;
+
+public class Printf {
+
+    public static void main(String[] args) {
+
+        System.out.printf("%-17s %s %n", "Nama", "Alamat");
+        System.out.printf("%-17s %s %n", "Fayumi", "Malunda");
+        System.out.printf("%-17s %s %n", "Atisa W", "Kalukku");
+        System.out.printf("%-17s %s %n", "Ananda Dea", "Kalukku");
+        System.out.printf("%-17s %s %n", "Yulia SP", "Papalang");
+        System.out.printf("%-17s %s %n", "Bung Wanti", "Sisango");
+        System.out.printf("%-17s %s %n", "Serly", "Pasangkayu");
+
+    }
+}
