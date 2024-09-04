877227adb60ae0e77c3f182ee1d2a82c25ac534b
F. AyuIqraami <161196084+AyuIqraami@users.noreply.github.com>
2024-09-04 16:23:07 +0800

    Day3.java

---
+++ Day3.java
@@ -0,0 +1,17 @@
+package Day3;
+
+public class Cetak {
+
+    public static void main(String[] args) {
+
+        System.out.println("+----------+-------------+----------------+");
+        System.out.printf("|%-10s| %-12s| %-15s| %n", " Nama BG", "Tahun Debut", "Jumlah Member");
+        System.out.println("+----------+-------------+----------------+");
+        System.out.printf("|%-10s| %-12d| %-15d| %n", " Bangtan", 2013, 7);
+        System.out.printf("|%-10s| %-12d| %-15d| %n", " TXT", 2019, 5);
+        System.out.printf("|%-10s| %-12d| %-15d| %n", " Enhypen", 2020, 7);
+        System.out.printf("|%-10s| %-12d| %-15d| %n", " SVT", 2015, 13);
+        System.out.println("+----------+-------------+----------------+");
+
+    }
+}
