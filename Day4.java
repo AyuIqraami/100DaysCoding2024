03b5c987059595dd42bb0ddd0b38b052e87a30ea
F. AyuIqraami <161196084+AyuIqraami@users.noreply.github.com>
2024-09-05 20:33:01 +0800

    Day4.java

---
+++ Day4.java
@@ -0,0 +1,21 @@
+package Day4;
+
+public class VariabelFinal {
+
+    public static void main(String[] args) {
+        final String BG, Biased, en;
+        final int ageb1, ageb2;
+
+        BG = "Enhypen";
+        Biased = "HEEWON, but still luv ot7";
+        ageb1 = 22;
+        ageb2 = 20;
+
+        System.out.println("Nama BG : " + BG);
+        System.out.println("Biased  : " + Biased);
+        System.out.println("Umur B1 : " + ageb1 + "yo");
+        System.out.println("Umur B2 : " + ageb2 + "yo");
+
+    }
+
+}
