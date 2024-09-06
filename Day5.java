072ef38beb7422de41b8f54d8c946a6ea23245b4
F. AyuIqraami <161196084+AyuIqraami@users.noreply.github.com>
2024-09-06 15:29:52 +0800

    Day5.java

---
+++ Day5.java
@@ -0,0 +1,17 @@
+package Day5;
+
+public class Komentar {
+    //Komentar merupakan bagian dari kode program yang tidak dieksekusi mesin.
+    //Komentar digunakan untuk menjelaskan atau memberikan informasi tambahan kepada programmer yang membaca kode tersebut.
+    //Salah satu jenis komentar adalah Single-Line Comment yang menggunakan // Sebelum teks komentar.
+
+    public static void main(String[] args) { //ini merupakan method main
+
+        System.out.println("Hello World");//menampilkan output ke konsol menggunakan metode println
+
+        System.out.print("Day-5");//menampilkan output ke konsol menggunakan metode print
+
+        System.out.printf(" %s %n", "of coding 100 days");//menampilkan output ke konsol menggunakan metode printf
+
+    }
+}
