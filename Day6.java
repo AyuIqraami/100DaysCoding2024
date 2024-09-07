d2cd3c9e326245a476e3691802016d34cfad3dab
F. AyuIqraami <161196084+AyuIqraami@users.noreply.github.com>
2024-09-07 17:31:40 +0800

    Day6.java

---
+++ Day6.java
@@ -0,0 +1,28 @@
+package Day6;
+
+public class DeklarasidanInisialisasi {
+
+    public static void main(String[] args) {
+        //Deklarasi Variabel
+        int number;
+        byte data1;
+        short data2;
+
+        //Inisialisasi variabel
+        number = 77;
+        data1 = 5;
+        data2 = 13;
+
+        //Dekalrasi dan inisialisasi Variabel
+        int both1 = 32;
+        byte both2 = 17;
+        short both3 = 11;
+
+        //Menggunakan variabel dalam output
+        System.out.println("value dari variabel both1 : " + both1);
+        System.out.println("value dari variabel both2 : " + both2);
+        System.out.println("value dari variabel both3 : " + both3);
+
+    }
+
+}
