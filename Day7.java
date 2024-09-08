6a0e65f9f88015ae9e25cc5f1965f3fc2a838a3d
F. AyuIqraami <161196084+AyuIqraami@users.noreply.github.com>
2024-09-08 17:57:37 +0800

    Day7.java

---
+++ Day7.java
@@ -0,0 +1,22 @@
+package Day7;
+
+public class UpdateVariabel {
+
+    public static void main(String[] args) {
+        //Deklarasi dan inisialisasi
+        int data1 = 89;
+        byte data2 = 35;
+        short data3 = 77;
+        System.out.print("Nilai Awal : ");
+        System.out.printf("%d %d %d %n", data1, data2, data3);
+
+        //Memperbarui nilai variabel
+        data1 = 775;
+        data2 = 4;
+        data3 = 135;
+        System.out.print("After Updating : ");
+        System.out.printf("%d %d %d %n", data1, data2, data3);
+
+    }
+
+}
