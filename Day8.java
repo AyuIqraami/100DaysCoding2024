133a1a4d85cb631a3a16292d4eabd8d8d70ed4af
F. AyuIqraami <161196084+AyuIqraami@users.noreply.github.com>
2024-09-09 20:49:12 +0800

    Day8.java

---
+++ Day8.java
@@ -0,0 +1,20 @@
+package Day8;
+
+public class UpdateVariabel_TipeDataString {
+    public static void main(String[] args) {
+        String fav = "Hybe Boy Group : ";
+        String BG1 = "Bangtan";
+        String BG2 = "SVT";
+        System.out.print(fav);
+        System.out.print(BG1+" ");
+        System.out.println(BG2);
+        
+        fav = "another BG : ";
+        BG1 = "TXT";
+        BG2 = "ENHYPEN";
+        System.out.print(fav);
+        System.out.print(BG1+" ");
+        System.out.println(BG2);
+    }
+
+}
