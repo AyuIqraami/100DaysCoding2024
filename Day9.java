41f46284b3ead7a315811f6b0428aaeb21abfe8e
F. AyuIqraami <161196084+AyuIqraami@users.noreply.github.com>
2024-09-10 19:50:44 +0800

    Day9.java

---
+++ Day9.java
@@ -0,0 +1,12 @@
+package Day9;
+
+public class final_variabel {
+
+    public static void main(String[] args) {
+        //final variabel merupakan variabel yang nilainya tidak dapat dirubah
+        final byte biner1 = 0;
+        final byte biner2 = 1;
+        System.out.println("Bilangan biner : " + biner1 + " dan " + biner2);
+
+    }
+}
