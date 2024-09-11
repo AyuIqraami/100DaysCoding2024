d9a6a0cf838a8cebef0de0daf60111f55cb38170
F. AyuIqraami <161196084+AyuIqraami@users.noreply.github.com>
2024-09-11 20:12:20 +0800

    Day10.java

---
+++ Day10.java
@@ -0,0 +1,12 @@
+package Day10;
+
+public class TipeData_byte {
+
+    public static void main(String[] args) {
+        byte data1, data2;
+        data1 = -128;
+        data2 = 127;
+        System.out.println("range tipe data byte : " + data1 + " hingga " + data2);
+    }
+
+}
