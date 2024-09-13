76c2a2dff4b079b6bd8e4d9d07a21d3a4404f7cf
F. AyuIqraami <161196084+AyuIqraami@users.noreply.github.com>
2024-09-13 19:10:51 +0800

    Day12.java

---
+++ Day12.java
@@ -0,0 +1,20 @@
+package Day12;
+
+public class TipeData_integer {
+
+    public static void main(String[] args) {
+        //deklarasi variabel dengan tipe data int
+        int data1, data2, data3;
+        
+        //inisialisasi
+        data1 = 77;
+        data2 = 13;
+        data3 = 5;
+        
+        //mencetak nilai variabel menggunakan println
+        System.out.println("Value of data1 : "+data1);
+        System.out.println("Value of data2 : "+data2);
+        System.out.println("Value of data3 : "+data3);
+    }
+
+}
