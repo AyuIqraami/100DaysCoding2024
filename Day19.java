95a84312697680fed1db34fdc63acf0f880ec8dd
F. AyuIqraami <161196084+AyuIqraami@users.noreply.github.com>
2024-09-20 19:09:00 +0800

    Day19.java

---
+++ Day19.java
@@ -0,0 +1,20 @@
+package Day19;
+
+public class OperatorPenugasan1 {
+
+    public static void main(String[] args) {
+        //pengisian nilai
+        int data1 = 5;
+        System.out.println("Value of data1 = " + data1);
+
+        //pengisian dan penambahan nilai
+        int data2 = 35;
+        data2 += 7;
+        //pengisian dan pengurangan nilai
+        int data3 = 77;
+        data3 -= 13;
+
+        System.out.println("value of data2 = " + data2);
+        System.out.println("Value of data3 = " + data3);
+    }
+}
