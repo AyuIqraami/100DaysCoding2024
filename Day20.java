780fbac64922d6b4211edfc1291d9bd1efc29f1e
F. AyuIqraami <161196084+AyuIqraami@users.noreply.github.com>
2024-09-21 20:02:44 +0800

    Day20.java

---
+++ Day20.java
@@ -0,0 +1,17 @@
+package Day20;
+
+public class OperatorPenugusan2 {
+
+    public static void main(String[] args) {
+        //pengisian dan perkalian
+        double data1 = 7;
+        data1 *= 2;
+        
+        //pengisian dan pembagian
+        int data2 = 135;
+        data2 /= 4;
+
+        System.out.println("Value of data1 = " + data1);
+        System.out.println("Value of data2 = " + data2);
+    }
+}
