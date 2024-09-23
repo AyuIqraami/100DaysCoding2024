9724cb8c980b483393c8039b3c2b6a77552c8744
F. AyuIqraami <161196084+AyuIqraami@users.noreply.github.com>
2024-09-23 19:55:31 +0800

    Day22.java

---
+++ Day22.java
@@ -0,0 +1,14 @@
+package Day22;
+
+public class OperatorPerbandingan1 {
+
+    public static void main(String[] args) {
+        //Operator perbandingan : lebih besar dan lebih kecil 
+        int a = 35;
+        int b = 45;
+        boolean hasil1 = a < b;
+        boolean hasil2 = a > b;
+        System.out.println(hasil1);
+        System.out.println(hasil2);
+    }
+}
