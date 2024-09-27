fbc53f2cc322e9084fb3e7d3356c2f32b83e8391
F. AyuIqraami <161196084+AyuIqraami@users.noreply.github.com>
2024-09-27 21:32:33 +0800

    Day26.java

---
+++ Day26.java
@@ -0,0 +1,15 @@
+package fromDay25Till_THE_END;
+
+public class KonversiString3 {
+
+    public static void main(String[] args) {
+        //konversi String ke tipe data double dan float
+        String a = "35.7";
+        double b = Double.parseDouble(a);
+        System.out.println(a);
+
+        String e = "13.7";
+        float c = Float.parseFloat(e);
+        System.out.println(c);
+    }
+}
