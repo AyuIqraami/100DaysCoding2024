9c79b427973c140a8d65081fae75c458eae97568
F. AyuIqraami <161196084+AyuIqraami@users.noreply.github.com>
2024-09-29 21:52:30 +0800

    Day28.java

---
+++ Day28.java
@@ -0,0 +1,15 @@
+package fromDay25Till_THE_END;
+
+public class Day28_Boolean {
+
+    public static void main(String[] args) {
+        //Tipe Data Reference : Boolean 
+        Boolean data1;
+        data1 = Boolean.TRUE;
+        System.out.println("Value of Boolean before updating = " + data1);
+        data1 = Boolean.FALSE;
+        System.out.println("Value of Boolean after updating = " + data1);
+        Boolean data2 = null;
+        System.out.println("Value of data2 = " + data2);
+    }
+}
