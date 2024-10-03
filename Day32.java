2d064619c2b2582282fed387028c3e7d19e58e86
F. AyuIqraami <161196084+AyuIqraami@users.noreply.github.com>
2024-10-03 19:48:43 +0800

    Day32.java

---
+++ Day32.java
@@ -0,0 +1,14 @@
+package fromDay25Till_THE_END;
+
+public class Day32_TypeCasting {
+
+    public static void main(String[] args) {
+        int a = 77;
+        double b = (double) a;
+        System.out.println("Hasil casting int to double : "+b);
+        
+        double c = 13.5;
+        int d = (int) c;
+        System.out.println("Hasil casting double to int : "+d);
+    }
+}
