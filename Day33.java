4c49ace8d8ca4d14018e2af2f1319c67f847030d
F. AyuIqraami <161196084+AyuIqraami@users.noreply.github.com>
2024-10-04 20:06:40 +0800

    Day33.java

---
+++ Day33.java
@@ -0,0 +1,14 @@
+package fromDay25Till_THE_END;
+
+public class Day33_PrimitifToString {
+
+    public static void main(String[] args) {
+        int a = 123;
+        String b = String.valueOf(a);
+        System.out.println(b);
+
+        double c = 123.45;
+        String d = String.valueOf(c);
+        System.out.println(d);
+    }
+}
