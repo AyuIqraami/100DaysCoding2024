c77339f5dfae77a605880e3697ab90b9ad41566b
F. AyuIqraami <161196084+AyuIqraami@users.noreply.github.com>
2024-10-08 21:15:44 +0800

    Day37.java

---
+++ Day37.java
@@ -0,0 +1,21 @@
+package fromDay25Till_THE_END;
+
+public class Day37_OperatorPenugasan1 {
+
+    public static void main(String[] args) {
+        //Pengisian Nilai
+        int a = 23;
+        System.out.println("nilai variabel a = " + a);
+
+        //Pengisian dan Penambahan
+        int b = 7;
+        b += 7;
+        System.out.println("nilai variabel b = " + b);
+
+        //Pengisian dan Pengurangan
+        int c = 13;
+        c -= 5;
+        System.out.println("nilai variabel c = " + c);
+    }
+
+}
