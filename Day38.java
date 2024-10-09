2a1c76737892bd360565b763f769fe5dbb073ec5
F. AyuIqraami <161196084+AyuIqraami@users.noreply.github.com>
2024-10-09 20:28:13 +0800

    Day38.java

---
+++ Day38.java
@@ -0,0 +1,16 @@
+package fromDay25Till_THE_END;
+
+public class Day38_OperatorPenugasan2 {
+
+    public static void main(String[] args) {
+        //Pengisian dan Perkalian
+        int a = 7;
+        a *= 2;
+        System.out.println("Nilai variabel a = " + a);
+
+        //Pengisian dan Pembagian
+        int b = 22;
+        b /= 4;
+        System.out.println("Nilai variabel b = " + b);
+    }
+}
