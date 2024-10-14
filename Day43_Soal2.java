1ebdd3f78bb055ecd233f0526cf52719de02a783
F. AyuIqraami <161196084+AyuIqraami@users.noreply.github.com>
2024-10-14 17:36:30 +0800

    Day43_Soal2.java

---
+++ Day43_Soal2.java
@@ -0,0 +1,22 @@
+package fromDay25Till_THE_END;
+
+public class Day43_Soal2 {
+
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 20;
+
+        System.out.println(b);
+        b -= a;
+        System.out.println(b);
+        b *= a;
+        System.out.println(b);
+        b -= a;
+        System.out.println(b);
+        b /= a;
+        System.out.println(b);
+        b += a;
+        System.out.println(b);
+
+    }
+}
