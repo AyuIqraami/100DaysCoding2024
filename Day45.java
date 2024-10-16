d5171c97a78632898c9b86bfebf0480f9f78d58f
F. AyuIqraami <161196084+AyuIqraami@users.noreply.github.com>
2024-10-16 19:32:36 +0800

    Day45.java

---
+++ Day45.java
@@ -0,0 +1,12 @@
+package fromDay25Till_THE_END;
+
+public class Day45_OperatorLogikaOR {
+
+    public static void main(String[] args) {
+        boolean a = false;
+        boolean b = true;
+        boolean c = a || b;
+        System.out.println("a || b = " + c);
+
+    }
+}
