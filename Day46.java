7617d8a6fe9ed24025c5c74d49ba973cf780ca15
F. AyuIqraami <161196084+AyuIqraami@users.noreply.github.com>
2024-10-17 20:16:42 +0800

    Day46.java

---
+++ Day46.java
@@ -0,0 +1,17 @@
+package fromDay25Till_THE_END;
+
+public class Day46_OperatorLogika_Negasikebalikan {
+
+    public static void main(String[] args) {
+
+        boolean a = true;
+        boolean b = false;
+
+        // operator negasi
+        boolean negasiTrue = !a;
+        boolean negasiFalse = !b;
+
+        System.out.println("Negasi dari true \t: " + negasiTrue);
+        System.out.println("Negasi dari false\t: " + negasiFalse);
+    }
+}
