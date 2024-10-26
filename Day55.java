542173b4a25eb95ea42a9321b60255474f6b2a2a
F. AyuIqraami <161196084+AyuIqraami@users.noreply.github.com>
2024-10-26 23:54:07 +0800

    Day55.java

---
+++ Day55.java
@@ -0,0 +1,11 @@
+package fromDay25Till_THE_END;
+
+public class Day55 {
+
+    public static void main(String[] args) {
+        // Looping dari 10 ke 1
+        for (int i = 10; i >= 1; i--) {
+            System.out.println("Nilai i: " + i);
+        }
+    }
+}
