a5b8af30ec108c29e13ed436f7e5680ffc4b11eb
F. AyuIqraami <161196084+AyuIqraami@users.noreply.github.com>
2024-10-31 19:55:23 +0800

    Day60.java

---
+++ Day60.java
@@ -0,0 +1,13 @@
+package fromDay25Till_THE_END;
+
+public class Day60_Looping_doWhile {
+
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println("Nilai i: " + i);
+            i++;
+        } while (i < 5);
+
+    }
+}
