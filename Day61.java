8b6484ae49fdfa1ea4415fccd7440bc5c99eb841
F. AyuIqraami <161196084+AyuIqraami@users.noreply.github.com>
2024-11-01 20:24:20 +0800

    Day61.java

---
+++ Day61.java
@@ -0,0 +1,12 @@
+package fromDay25Till_THE_END;
+
+public class Day61_InfiniteLoop {
+
+    public static void main(String[] args) {
+        int a = 0;
+        while (a < 7){
+            System.out.println("INFINITE LOOP!!");
+        }     
+    }
+}
+
