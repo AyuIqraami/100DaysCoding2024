66e9e30f0740427345e928d46ff1eb00d64805b4
F. AyuIqraami <161196084+AyuIqraami@users.noreply.github.com>
2024-11-03 20:19:44 +0800

    Day63.java

---
+++ Day63.java
@@ -0,0 +1,13 @@
+package fromDay25Till_THE_END;
+
+public class Day63_Keyword_continue {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 != 0) {
+                continue; 
+            }
+            System.out.println(i); 
+        }
+    }
+}
