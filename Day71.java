10c36958b87008eec79b25007183fbd6d6ff637d
F. AyuIqraami <161196084+AyuIqraami@users.noreply.github.com>
2024-11-11 20:15:29 +0800

    Day71.java

---
+++ Day71.java
@@ -0,0 +1,12 @@
+package fromDay25Till_THE_END;
+
+public class Day71_MethodStatic {
+
+    public static void cetak(String pesan) {
+        System.out.println(pesan);
+    }
+
+    public static void main(String[] args) {
+        cetak("Hello World");
+    }
+}
