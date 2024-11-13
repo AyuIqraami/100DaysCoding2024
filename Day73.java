a3271741e23961b0cea9f350df691c8ae518b31b
F. AyuIqraami <161196084+AyuIqraami@users.noreply.github.com>
2024-11-13 23:47:25 +0800

    Day73.java

---
+++ Day73.java
@@ -0,0 +1,12 @@
+package fromDay25Till_THE_END;
+
+public class Day73_ {
+
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+
+        // Mengambil karakter pertama
+        char ch1 = str.charAt(0);  // Karakter pada indeks 0 (H)
+        System.out.println("Karakter pertama: " + ch1);
+    }
+}
