f8dd5c3e0e8c19b30a6585fcaa7a2a6c675307c7
F. AyuIqraami <161196084+AyuIqraami@users.noreply.github.com>
2024-11-14 20:20:11 +0800

    Day74.java

---
+++ Day74.java
@@ -0,0 +1,15 @@
+package fromDay25Till_THE_END;
+
+import java.util.Scanner;
+
+public class Day74_MethodLength {
+
+    public static void main(String[] args) {
+        Scanner ayu = new Scanner(System.in);
+        System.out.print("Masukkan Kalimat : ");
+        String kalimat = ayu.nextLine();
+        int panjang = kalimat.length();
+        System.out.println("========================================");
+        System.out.println("Panjang kalimat yang di-input : " + panjang);
+    }
+}
