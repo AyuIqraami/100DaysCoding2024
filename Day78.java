6aaf694908607d29ad0f522c47b028224ccf1d5a
F. AyuIqraami <161196084+AyuIqraami@users.noreply.github.com>
2024-11-18 21:02:27 +0800

    Day78.java

---
+++ Day78.java
@@ -0,0 +1,16 @@
+package fromDay25Till_THE_END;
+
+public class Day78_equalsIgnoreCase {
+
+    public static void main(String[] args) {
+        String a = "Ayu";
+        String b = "ayu";
+
+        if (a.equalsIgnoreCase(b)) {
+            System.out.println("a dan b memiliki nilai yang sama");
+
+        } else {
+            System.out.println("a dan b memiliki nilai yang berbeda");
+        }
+    }
+}
