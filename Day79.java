c34b817262be581a4c66e5143fca7ee9823db513
F. AyuIqraami <161196084+AyuIqraami@users.noreply.github.com>
2024-11-19 20:57:15 +0800

    Day79.java

---
+++ Day79.java
@@ -0,0 +1,15 @@
+package fromDay25Till_THE_END;
+
+import java.util.Scanner;
+
+public class Day79_toString {
+
+    public static void main(String[] args) {
+        Scanner ayu = new Scanner(System.in);
+
+        System.out.print("Masukkan Angka : ");
+        int angka = ayu.nextInt();
+
+        System.out.println(Integer.toString(angka));
+    }
+}
