9928b06b496682a05774c7ed42b5cd7bad76cda0
F. AyuIqraami <161196084+AyuIqraami@users.noreply.github.com>
2024-11-20 21:34:49 +0800

    Day80.java

---
+++ Day80.java
@@ -0,0 +1,12 @@
+package fromDay25Till_THE_END;
+
+public class Day80_trim {
+
+    public static void main(String[] args) {
+        String str = "   Hello, World!   ";
+        String hasil = str.trim();
+
+        System.out.println("Sebelum trim: '" + str + "");
+        System.out.println("Setelah trim: '" + hasil + "'");
+    }
+}
