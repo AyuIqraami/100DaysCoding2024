11a5ab8c2d8628b457a1256effac897be979f5a2
F. AyuIqraami <161196084+AyuIqraami@users.noreply.github.com>
2024-11-21 20:00:22 +0800

    Day81.java

---
+++ Day81.java
@@ -0,0 +1,16 @@
+package fromDay25Till_THE_END;
+
+import java.util.Scanner;
+
+public class Day81_valueof {
+
+    public static void main(String[] args) {
+        Scanner ayu = new Scanner(System.in);
+        
+        System.out.print("Input Angka : ");
+        long angka = ayu.nextLong();
+        
+        String hasil = String.valueOf(angka);
+        System.out.println("Nilai Setelah di konversi : " + hasil);
+    }
+}
