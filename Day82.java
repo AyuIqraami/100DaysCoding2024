5c23bcd6d7a7ca25216fb2716415ee4450458296
F. AyuIqraami <161196084+AyuIqraami@users.noreply.github.com>
2024-11-22 16:50:53 +0800

    Day82.java

---
+++ Day82.java
@@ -0,0 +1,16 @@
+package fromDay25Till_THE_END;
+
+public class Day82_MethodCeilFloorRound {
+
+    public static void main(String[] args) {
+        double angka = 7.5;
+
+        double hasil1 = Math.ceil(angka);
+        double hasil2 = Math.floor(angka);
+        long hasil3 = Math.round(angka);
+
+        System.out.println("Hasil Method Ceil  : " + hasil1);
+        System.out.println("Hasil Method Floor : " + hasil2);
+        System.out.println("Hasil Method Round : " + hasil3);
+    }
+}
