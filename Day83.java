e9ab2a021367dd4feb701f33320e5350438fe078
F. AyuIqraami <161196084+AyuIqraami@users.noreply.github.com>
2024-11-23 20:24:34 +0800

    Day83.java

---
+++ Day83.java
@@ -0,0 +1,16 @@
+package fromDay25Till_THE_END;
+
+import java.util.Scanner;
+
+public class Day83_ObjekArray1D {
+
+    public static void main(String[] args) {
+        Scanner ayu = new Scanner(System.in);
+
+        System.out.print("Masukkan ukuran array : ");
+        int ukuran = ayu.nextInt();
+        int array[] = new int[ukuran];
+
+        String nama[] = new String[3];
+    }
+}
