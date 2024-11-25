d993a6641960fd36cc5fe452c60d451b8412180c
F. AyuIqraami <161196084+AyuIqraami@users.noreply.github.com>
2024-11-25 20:33:08 +0800

    Day85.java

---
+++ Day85.java
@@ -0,0 +1,13 @@
+package fromDay25Till_THE_END;
+
+public class Day85_MengisiElemenArray1D {
+
+    public static void main(String[] args) {
+        String nama[] = new String[3];
+        nama[0] = "Ayu";
+        nama[1] = "Tisa";
+        nama[2] = "Yulia";
+
+        int angka[] = {7, 7, 5, 12};
+    }
+}
